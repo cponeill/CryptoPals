0184d198ca2a62326273c5d192ef97145c0decc5
cponeill <caseyoneill78@hotmail.com>
2016-03-06 08:09:12 -0800

    fixed and added comments

--- Basics/challenge_03.py
+++ Basics/challenge_03.py
@@ -1,14 +1,14 @@
 x = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
 
-# fix the x to xs on line 5
+# fix the x to xs on line 5 <fixed>
 # add comments
 def score_plaintext(xs):
-	'''add comments'''
-	letters = filter(lambda x: 'a'<=x<='z' or 'A'<=x<='Z', x)
+	'''Finds the score'''
+	letters = filter(lambda x: 'a'<=x<='z' or 'A'<=x<='Z', xs)
 	return float(len(letters)) / len(xs)
 
 def get_max_single_char_xor(xs):
-	'''add comments'''
+	'''loops through string and xors with the score'''
 	result = []
 	for i in range(256):
 		chrs = [chr(ord(xs) ^ i) for xs in x.decode('hex')]
