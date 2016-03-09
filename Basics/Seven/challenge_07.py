3e014e6e0bef0705e1db69970a753121581464fb
cponeill <caseyoneill78@hotmail.com>
2016-03-08 16:23:06 -0800

    adding code

--- Basics/Seven/challenge_07.py
+++ Basics/Seven/challenge_07.py
@@ -1,3 +1,16 @@
 """
-Add challenge 07 code here.
+Add comments to understand code.
 """
+
+
+from Crypto.Cipher import AES
+from binascii import a2b_base64
+
+def AES_128_ECB_decrypt(data, key):
+  cipher = AES.new(key, AES.MODE_ECB)
+  return cipher.decrypt(data)
+
+filename = 'text_seven.txt'
+key = 'YELLOW SUBMARINE'
+data = a2b_base64(''.join(line.strip() for line in open(filename)))
+print AES_128_ECB_decrypt(data, key)
